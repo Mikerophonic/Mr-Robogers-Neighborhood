7b1014fab01a0a142d639ca6123b6d110ca0b56f
Mikerophonic <micgcon@gmail.com>
2023-06-29 10:02:16 -0700

    pass second test

--- js/scripts.js
+++ js/scripts.js
@@ -2,4 +2,7 @@ function beepBoop(number) {
     if (number.trim().length === 0) {
         return "Please enter a number."
     }
+    if (isNaN(Number(number)) === true) {
+        return "Please enter a number."
+    }
 }
